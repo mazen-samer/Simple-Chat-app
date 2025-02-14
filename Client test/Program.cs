86197908538c3ab7c4ac9fdf3f8d1ef3985a2671
mazen-samer <mazen.samer.2000@gmail.com>
2025-02-15 00:48:47 +0200

    first commit

---
+++ Client test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Client_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(IPAddress.Parse("127.0.0.1"), 5001);
+            StreamWriter writer = new StreamWriter(tcpClient.GetStream()) { AutoFlush = true };
+            StreamReader reader = new StreamReader(tcpClient.GetStream());
+
+            Thread readThread = new Thread(() =>
+            {
+                while (tcpClient.Connected)
+                {
+                    try
+                    {
+                        string response = reader.ReadLine();
+                        if (response != null)
+                        {
+                            Console.WriteLine(response);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                }
+            });
+            readThread.Start();
+
+            while (tcpClient.Connected)
+            {
+                string s = Console.ReadLine();
+                if (s == "Exit")
+                    break;
+                Console.Write("==>");
+                writer.WriteLine(s);
+            }
+
+            tcpClient.Close();
+        }
+    }
+}
---
+++ TCP Server_Client Demo/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Server_Client_Demo
+{
+    internal class Program
+    {
+        private static List<(TcpClient client, string id)> clients = new List<(TcpClient, string)>();
+        private static int clientCounter = 0;
+
+        public static void ProcessClientRequests(object? client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            string clientId = "Client " + (++clientCounter);
+            clients.Add((tcpClient, clientId));
+            StreamReader reader = new StreamReader(tcpClient.GetStream());
+            StreamWriter writer = new StreamWriter(tcpClient.GetStream()) { AutoFlush = true };
+            string message;
+
+            try
+            {
+                writer.WriteLine($"Welcome {clientId}");
+                while ((message = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine($"From {clientId} => " + message);
+                    if (message.Equals("Exit"))
+                        break;
+
+                    foreach (var clientInstance in clients)
+                    {
+                        if (clientInstance.client != tcpClient)
+                        {
+                            StreamWriter clientWriter = new StreamWriter(clientInstance.client.GetStream()) { AutoFlush = true };
+                            clientWriter.WriteLine($"{clientId} => " + message);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                clients.Remove((tcpClient, clientId));
+                reader.Close();
+                writer.Close();
+                tcpClient.Close();
+                Console.WriteLine("Connection closed with client: " + clientId);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TcpListener listener = null;
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 5001);
+                listener.Start();
+                Console.WriteLine("Server has started...");
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a client...");
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine("Client connected.");
+                    Thread t = new Thread(() => ProcessClientRequests(client));
+                    t.Start();
+                }
+            }
+            catch (Exception? ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                listener?.Stop();
+            }
+        }
+    }
+}
